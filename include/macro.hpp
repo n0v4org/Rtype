1d32c480f065074c83139b09c5a88298879001e9
N0v4 <antoine.le@epitech.eu>
2024-11-18 10:31:17 +0100

    feat(archi): main && macro

---
+++ include/macro.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Rtype
+** File description:
+** macro
+*/
+
+#ifndef MACRO_HPP_
+    #define MACRO_HPP_
+    #define OK 0
+    #define ERROR 84
+
+
+#endif /* !MACRO_HPP_ */
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** Rtype
+** File description:
+** main
+*/
+
+#include "../include/macro.hpp"
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "hello world!" << std::endl;
+    return OK;
+}
