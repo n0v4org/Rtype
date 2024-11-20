0c0692c840ed85b155372c37793cd42fd78b1a7d
N0v4 <antoine.le@epitech.eu>
2024-11-20 16:19:38 +0100

    feat(archi): update 3rd party librarii handling with vcpkg && unit test integrate with Gtest in vcpkg

--- utils/build.sh
+++ utils/build.sh
@@ -4,8 +4,10 @@ set -e
 
 echo "go in build directory"
 
+rm -rf build
+mkdir build
 cd build/
 
-cmake ../ && make
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=~$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake && make
 
-mv server/r-type_server client/r-type_client ../ 
+mv server/r-type_server  server/r-type_server_tests client/r-type_client ../ 
