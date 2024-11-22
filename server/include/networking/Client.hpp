ab0aa7ac762cb9e71735ada12d9e08335cb46366
N0v4 <antoine.le@epitech.eu>
2024-11-22 22:23:31 +0100

    feat(server): setup of multithreading -> create a new thread at each new endpoint and store it in a map

---
+++ server/include/networking/Client.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** Rtype
+** File description:
+** Client
+*/
+
+#ifndef SERVER_INCLUDE_NETWORKING_CLIENT_HPP_
+#define SERVER_INCLUDE_NETWORKING_CLIENT_HPP_
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+namespace rtype {
+  namespace net {
+class Client {
+    public:
+        void operator()(udp::endpoint);
+
+    protected:
+    private:
+        udp::endpoint _client_endpoint;
+};
+  }  // namespace net
+}  // namespace rtype
+
+#endif /* SERVER_INCLUDE_NETWORKING_CLIENT_HPP_ */
---
+++ server/src/networking/Client.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** Rtype
+** File description:
+** Client
+*/
+
+#include "networking/Client.hpp"
+
+#include <iostream>
+
+#include <boost/asio.hpp>
+
+
+namespace rtype {
+  namespace net {
+
+void Client::operator()(udp::endpoint endpoint) {
+    std::cout << "in operator " << endpoint<<  std::endl;
+    _client_endpoint = endpoint;
+}
+
+  }  // namespace net
+}  // namespace rtype
