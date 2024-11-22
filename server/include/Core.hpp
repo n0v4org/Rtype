4e0e021d4efc7b1f89d7959cefd8d2b1f4a82261
n0v46027 <n0v46027@users.noreply.github.com>
2024-11-22 15:22:44 +0000

    [MegaLinter] Apply linters fixes

--- server/include/Core.hpp
+++ server/include/Core.hpp
@@ -7,25 +7,25 @@
 
 #ifndef SERVER_INCLUDE_CORE_HPP_
 #define SERVER_INCLUDE_CORE_HPP_
-#include <memory>
 #include <boost/asio.hpp>
+#include <memory>
 #include "Arguments.hpp"
 #include "networking/Server.hpp"
 
 namespace rtype {
 
-class Core {
-public:
-  Core(char *[], int);
-  void run();
-  ~Core();
+  class Core {
+  public:
+    Core(char *[], int);
+    void run();
+    ~Core();
 
-protected:
-private:
-  std::unique_ptr<Arguments> _args;
-  std::unique_ptr<net::Server> _network;
-  boost::asio::io_context _io_context;
-};
+  protected:
+  private:
+    std::unique_ptr<Arguments> _args;
+    std::unique_ptr<net::Server> _network;
+    boost::asio::io_context _io_context;
+  };
 
 }  // namespace rtype
 
