e7936d360e90b2d949bb0bf6503c7b9738332918
Théo Fisch-Dewailly <theo.fisch-dewailly@epitech.eu>
2024-12-09 17:38:40 +0100

    feat: new proto functions

--- client/include/graphic/Sfml.cpp
+++ client/include/graphic/Sfml.cpp
@@ -42,8 +42,27 @@ namespace rtype{
   Sfml::~Sfml() {
   }
 
+  void Sfml::drawSprite() {};
+  void Sfml::drawText() {};
 
-  void Sfml::drawSprite() {}
-  void Sfml::drawText() {}
+
+  void storeAssetsPNG(std::string){};
+  void storeAssetsWAV(std::string){};
+  void storeAssetsTTF(std::string){};
+  void storeAssetsVERT(std::string){};
+
+  void playSound(std::string){};
+
+  void saveAnimation(std::pair<std::size_t,std::size_t>,std::pair<std::size_t,std::size_t>,float,std::chrono::milliseconds){};
+
+  void playOnce(std::string,std::string){};
+  void playLoop(std::string,std::string){};
+
+  void setAnimationSpeed(std::string,float){};
+  void setScale(std::string,std::pair<float,float>){};
+  void setPosition(std::string,std::pair<std::size_t,std::size_t>){};
+  void setRotation(std::string,float){};
+
+  void moveCamera(std::vector<float>){};
 
 }
