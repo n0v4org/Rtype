1d96efc260cdfcd3f968349d243ee0a8ea3ecfa0
Théo Fisch-Dewailly <theo.fisch-dewailly@epitech.eu>
2024-12-10 21:15:38 +0100

    fix: update Adisplay

--- client/include/graphic/ADisplayModule.hpp
+++ client/include/graphic/ADisplayModule.hpp
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2024
 ** Rtype
 ** File description:
-** ADisplayModule
+** IDisplayModule
 */
 
 #ifndef CLIENT_INCLUDE_GRAPHIC_ADISPLAYMODULE_HPP_
@@ -23,68 +23,59 @@ class ADisplayModule : IDisplayModule{
     virtual void refresh() = 0;
     virtual bool isOpen() = 0;
 
-    void loadAssets(std::string folder_path) override
-    {
-    	std::string pathname;
-      	std::string folder_name;
 
-       	bool status = false;
-        for (const auto &i: std::filesystem::directory_iterator(folder_path + "/sprites")) {
-          for (auto &[key, data]: _sprites) {
-            if (key == folder_path) {
-              status = true;
-            }
+    void loadAssets(std::string folder_path) override{
+      std::string pathname;
+      std::string folder_name;
+      bool status = false;
+      for (const auto &i: std::filesystem::directory_iterator(folder_path + "/sprites")) {
+        for (auto &[key, data]: _sprites) {
+          if (key == folder_path) {
+            status = true;
           }
-          if (status) {
-            break;
+        }
+        if (status) {
+          break;
+        }
+        storeAssetsPNG(i.path());
+      }
+      status = false;
+      for (const auto &i: std::filesystem::directory_iterator(folder_path + "/fonts")) {
+        for (auto &[key, data]: _fonts) {
+          if (key == folder_path) {
+            status = true;
           }
-          pathname = i.path();
-          folder_name = pathname.substr(pathname.find("sprites/") + 4);
-            storeAssetsPNG(pathname);
+        }
+        if (status) {
+          break;
+        }
+        storeAssetsTTF(i.path());
+      }
+      status = false;
+      for (const auto &i: std::filesystem::directory_iterator(folder_path + "/sounds")) {
+        for (auto &[key, data]: _sounds) {
+          if (key == folder_path) {
+            status = true;
           }
-          status = false;
-          for (const auto &i: std::filesystem::directory_iterator(folder_path + "/fonts")) {
-            for (auto &[key, data]: _fonts) {
-              if (key == folder_path) {
-                status = true;
-              }
-            }
-            if (status) {
-              break;
-            }
-            pathname =  i.path();
-            folder_name = pathname.substr(pathname.find("fonts/") + 6);
-            storeAssetsTTF(i.path());
+        }
+        if (status) {
+          break;
+        }
+        storeAssetsWAV(i.path());
+      }
+      status = false;
+      for (const auto &i: std::filesystem::directory_iterator(folder_path + "/shaders")) {
+        for (auto &[key, data]: _shaders) {
+          if (key == folder_path) {
+            status = true;
           }
-          status = false;
-          for (const auto &i: std::filesystem::directory_iterator(folder_path + "/sounds")) {
-            for (auto &[key, data]: _sounds) {
-              if (key == folder_path) {
-                status = true;
-              }
-            }
-            if (status) {
-              break;
-            }
-            pathname =  i.path();
-            folder_name = pathname.substr(pathname.find("sounds/") + 6);
-            storeAssetsWAV(i.path());
-          }
-          status = false;
-          for (const auto &i: std::filesystem::directory_iterator(folder_path + "/shaders")) {
-            for (auto &[key, data]: _shaders) {
-              if (key == folder_path) {
-                status = true;
-              }
-            }
-            if (status) {
-              break;
-            }
-            pathname =  i.path();
-            folder_name = pathname.substr(pathname.find("shaders/") + 6);
-            storeAssetsVERT(i.path());
-          }
-        };
+        }
+        if (status) {
+          break;
+        }
+        storeAssetsVERT(i.path());
+      }
+    };
     virtual void storeAssetsPNG(std::string)=0;
     virtual void storeAssetsWAV(std::string)=0;
     virtual void storeAssetsTTF(std::string)=0;
@@ -92,33 +83,26 @@ class ADisplayModule : IDisplayModule{
 
     virtual UserInput getEvent() = 0;
 
+    //virtual void drawSprite(DrawableSprite_t) = 0;
+    virtual void drawSprite(std::string, std::size_t, int, int, float, float, float, float) = 0;
+    //virtual void drawText(DrawableText_t) = 0;
+    virtual void drawText(std::string, std::string, std::size_t, int, int, float, float, float, float) = 0;
 
     virtual void playSound(std::string) = 0;
-    virtual void drawText() = 0;
-    virtual void drawSprite() = 0;
 
-    virtual void saveAnimation(std::pair<std::size_t,std::size_t>,std::pair<std::size_t,std::size_t>,float,std::chrono::milliseconds)=0;
+    //virtual void saveAnimation(std::string, Animation_t)=0;
+    virtual void saveAnimation(std::string, std::string, std::size_t, std::size_t, std::size_t, std::size_t) = 0;
 
-    virtual void playOnce(std::string,std::string)=0;
-    virtual void playLoop(std::string,std::string)=0;
-
-    virtual void setAnimationSpeed(std::string,float)=0;
-    virtual void setScale(std::string,std::pair<float,float>)=0;
-    virtual void setPosition(std::string,std::pair<std::size_t,std::size_t>)=0;
-    virtual void setRotation(std::string,float)=0;
-
-    virtual void moveCamera(std::vector<float>)=0;
+    virtual void moveCamera(int,int,int)=0;
 
   protected:
-  private:
-    std::map<std::string,Entity_t> _entities;
     std::map<std::string,Animation_t> _animations;
-    std::map<std::string, std::pair<std::string,std::string>> _texts;
 
     std::map<std::string,PNG> _sprites;
     std::map<std::string,WAV> _sounds;
     std::map<std::string,TTF> _fonts;
     std::map<std::string,VERT> _shaders;
+  private:
 
 };
 }
