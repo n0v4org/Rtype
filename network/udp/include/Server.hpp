b78f4833041ae3d66317e776d2d1b0ef5c536a44
N0v4 <antoine.le@epitech.eu>
2024-12-15 19:13:15 +0100

    feat(network/udp): build config for udp lib

---
+++ network/udp/include/Server.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** Rtype
+** File description:
+** Server
+*/
+
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+class Server {
+    public:
+        Server();
+        ~Server();
+
+    protected:
+    private:
+};
+
+#endif /* !SERVER_HPP_ */
---
+++ network/udp/src/Server.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** Rtype
+** File description:
+** Server
+*/
+
+#include "Server.hpp"
+
+Server::Server()
+{
+}
+
+Server::~Server()
+{
+}
