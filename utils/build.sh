a7c7b4334e7d29c3508d21fec95cb3acdd416cb1
IsaacGonzalezBottini <isaac.gonzalez-bottini@epitech.eu>
2024-12-28 19:28:08 +0100

    fix: sfml cross compil fix

--- utils/build.sh
+++ utils/build.sh
@@ -8,6 +8,6 @@ rm -rf build
 mkdir build
 cd build/
 
-cmake ../ -DCMAKE_PREFIX_PATH=C:\Users\Tristan\OneDrive\Documents\RType\Rtype\vcpkg_installed\x64-windows  && make
+cmake ../ -DCMAKE_PREFIX_PATH=~/delivery/tek3/RTYPE/vcpkg_installed/x64-linux && make
 
 #mv server/r-type_server  server/r-type_server_tests client/r-type_client client/r-type_client_tests ../ 
