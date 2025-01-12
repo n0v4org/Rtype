1ab3a0465d1f5137e21ab67d8fff9f9a60735057
Théo Fisch-Dewailly <theo.fisch-dewailly@epitech.eu>
2025-01-12 15:25:51 +0100

    feat: add main who work ?

--- client/game/game.cpp
+++ client/game/game.cpp
@@ -1,44 +1,35 @@
-/*
-** EPITECH PROJECT, 2024
-** RTYPE
-** File description:
-** game
-*/
-
-#include <string>
 #include "Engine.hpp"
-#include "Scenes.hpp"
-#include "systems.hpp"
-
+#include "Scenes.hpp"         
+#include "systems.hpp" 
 #include "modules/display/systems.hpp"
 #include "modules/movement/systems.hpp"
-#include "modules/network/systems.hpp"
+#include "components.hpp"
+#include "modules/display/components.hpp"
+#include "modules/movement/components.hpp"
 #include "modules/controller/systems.hpp"
-
-// #include "asio.hpp" 
-// #include "CommonCommands.hpp" 
+#include "events.hpp"
 
 void runClient(int /*sport*/, int /*cport*/, std::string /*ip*/) {
     zef::Engine engine;
 
-    engine.initGraphLib("../Assets", "");
+    engine.initGraphLib("Assets", "");
+
     engine.GraphLib->saveAnimation("menu", "menu", 0, 0, 1920, 1080);
+    engine.GraphLib->saveAnimation("play", "play", 0, 0, 200, 100);
 
     engine.registerComponent<zef::comp::position>();
     engine.registerComponent<zef::comp::drawable>();
-    engine.registerComponent<BackGround>(); 
+    engine.registerComponent<zef::comp::clickable>();
+    engine.registerComponent<zef::comp::event_listener>();
+    engine.registerComponent<BackGround>();
 
-    engine.addSystem<>("zefir", zef::sys::update_user_inputs);
-
+    engine.addSystem<zef::comp::clickable, zef::comp::position>("zefir", zef::sys::handleclickable);
     engine.addSystem<zef::comp::drawable>("zefir", zef::sys::update_animations);
-
-    engine.addSystem<zef::comp::drawable, zef::comp::position>(
-        "zefir", zef::sys::draw_drawables
-    );
+    engine.addSystem<zef::comp::drawable, zef::comp::position>("zefir", zef::sys::draw_drawables);
 
     engine.registerScene<MenuScene>("menu");
-
     engine.loadScene("menu");
 
-    engine.run(); 
+    engine.run();
 }
+
