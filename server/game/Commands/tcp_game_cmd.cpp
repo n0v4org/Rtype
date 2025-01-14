01b8124b4e1ecb1689632c51654e73a42a3e0881
N0v4 <antoine.le@epitech.eu>
2025-01-14 18:36:19 +0100

    fix(linter): linting errors

--- server/game/Commands/tcp_game_cmd.cpp
+++ server/game/Commands/tcp_game_cmd.cpp
@@ -11,7 +11,6 @@ namespace rtype {
   void Game::register_tcp_game_cmd() {
     _engine.registerCommandTcp("tester", [this](zef::Engine& test,
                                                        input_t input) {
-                                                         std::cout << "received : " << input.tcp_payload << std::endl;
       test.ServerSendTcp(input.id, "in game");
     });
   }
