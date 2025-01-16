2f3818a762f0cc2497586b19a37a646c2ec35c5c
Nicolas Rodiet <nicolas.rodiet@epitech.eu>
2025-01-16 06:10:50 +0100

    feat: adding a button with text argument patron

--- client/game/ButtonPatron.hpp
+++ client/game/ButtonPatron.hpp
@@ -56,4 +56,49 @@ class SoundBarPatron {
         }
 };
 
+class TextButtonPatron {
+public:
+  static void instanciate(zef::Engine &engine, const ecs::Entity &self,
+                          float x, float y,
+                          const std::string &spriteName,
+                          const std::string &text, const std::string &font, int textSize,
+                          std::function<void(zef::Engine &, size_t)> onClickCallback,
+                          float width = 200.0f, float height = 100.0f, float scale_w = 1.0f,
+                          float scale_h = 1.0f) {
+    engine.addEntityComponent<zef::comp::position>(self, x, y);
+
+    std::vector<zef::utils::hitbox> hb = {zef::utils::hitbox(0, 0, width * scale_w, height * scale_h)};
+    engine.addEntityComponent<zef::comp::clickable>(self, hb);
+
+    zef::comp::event_listener evtl;
+    evtl.setEvent<zef::evt::LeftclickRelease>(
+        [onClickCallback](zef::Engine &engine, size_t self, zef::evt::LeftclickRelease evt) {
+            onClickCallback(engine, self);
+        }
+    );
+    engine.addEntityComponent<zef::comp::event_listener>(self, evtl);
+
+    zef::comp::drawable dr;
+    dr.addAnimation(spriteName, 1, 200);
+    dr.playAnimationLoop(spriteName, 1);
+    dr.layer = 0;
+    dr.setScale(scale_w, scale_h);
+    engine.addEntityComponent<zef::comp::drawable>(self, dr);
+
+    zef::comp::drawableText txt;
+    txt.layer = 999;
+    txt.text = text;
+    txt.font = font;
+    txt.scaleX = scale_w;
+    txt.scaleY = scale_h;
+    txt.textSize = textSize;
+
+    engine.addEntityComponent<zef::comp::drawableText>(self, txt);
+
+  }
+};
+
+
+
+
 #endif // BUTTON_PATRON_HPP_
